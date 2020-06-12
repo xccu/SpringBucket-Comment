efdf6c5a9052fe9411d12f5760464a9290636d8d
Charlie <264672790@qq.com>
2020-06-12 20:16:30 +0800

    added comment

--- 32-simple-reactor-demo/src/main/java/com/example/simplereactordemo/SimplereactordemoApplication.java
+++ 32-simple-reactor-demo/src/main/java/com/example/simplereactordemo/SimplereactordemoApplication.java
@@ -18,32 +18,67 @@ public class SimplereactordemoApplication implements ApplicationRunner {
     }
 
 
+     /*
+     操作： Publisher(发布/生产者) / Subscriber（订阅/消费者）
+     集合序列：Flux [ 0..N ]  0或N个元素  Mono [ 0..1 ] 0或1个元素
+     遇到下一个元素时的操作： onNext()
+     序列完成时的操作：      onComplete()
+     发生错误时的操作：      onError()
+     */
+
+    /*
+    Backpressure
+    Subscription
+    onRequest()、onCancel()、onDispose() 设置请求个数，取消，终止订阅过程
+    */
+
+    /*
+    线程调度 Schedulers
+    线程操作： immediate() / single() / newSingle()        当前线程/复用线程/新启动线程
+    线程池操作：elastic() / parallel() / newParallel()     缓存池
+    错误处理
+    onError / onErrorReturn / onErrorResume              相当于try catch/遇到异常返回默认值/用特定lambda表达式进行异常处理
+    doOnError / doFinally                                发生错误时执行/相当于finally
+    */
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Flux.range(1, 6) //创建1到6的序列
-                //.publishOn(Schedulers.elastic()) //只请求了4个
-                .doOnRequest(n -> log.info("Request {} number", n)) //打印请求数 注意顺序造成的区别
+        //创建1到6的序列
+        Flux.range(1, 6)
+                //只请求了4个
+                //.publishOn(Schedulers.elastic())
+
+                //打印请求数 注意顺序造成的区别
+                .doOnRequest(n -> log.info("Request {} number", n))
 				//.publishOn(Schedulers.elastic())
-                .doOnComplete(() -> log.info("Publisher COMPLETE 1")) //序列1到6请求完成后打印Publisher COMPLETE 1
-                .map(i -> {//打印当前线程信息
-                    log.info("Publish {}, {}", Thread.currentThread(), i);
-                    //return 10 / (i - 3); //抛出异常测试
+
+                //序列1到6请求完成后打印Publisher COMPLETE 1
+                .doOnComplete(() -> log.info("Publisher COMPLETE 1"))
+
+                //map实现元素转换（映射）
+                .map(i -> {
+                    log.info("Publish {}, {}", Thread.currentThread(), i);              //打印当前线程信息
+                    //return 10 / (i - 3);                                              //抛出异常测试
 					return i;
                 })
                 .doOnComplete(() -> log.info("Publisher COMPLETE 2"))
 				.subscribeOn(Schedulers.single())
 
+                //抛出异常时，执行特定的lambda表达式
                 /*.onErrorResume(e -> {
-					log.error("Exception {}", e.toString()); //抛出异常信息
+					log.error("Exception {}", e.toString());
 					return Mono.just(-1);
 				})*/
 
-				.onErrorReturn(-1) //当异常发生时，返回指定的默认值并终止后续操作
-                .subscribe( //订阅
+                //当异常发生时，返回指定的默认值并终止后续操作
+				.onErrorReturn(-1)
+
+                //订阅
+                .subscribe(
                         i -> log.info("Subscribe {}: {}", Thread.currentThread(), i),
-                        e -> log.error("error {}", e.toString()), //输出异常
-                        () -> log.info("Subscriber COMPLETE"),
-						s -> s.request(4) //只取回4个请求
+                        e -> log.error("error {}", e.toString()),                       //输出异常
+                        () -> log.info("Subscriber COMPLETE")
+						//s -> s.request(4)                                             //只取回4个请求,且Publisher COMPLETE不输出
                 );
         Thread.sleep(2000);
     }
